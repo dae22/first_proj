80f11a96bf2c5bf2b0c29e57d0b7f4022b367b01
dae22 <dae-22@mail.ru>
2025-08-17 22:24:49 +0300

    Apartments app complete

--- apartments/urls.py
+++ apartments/urls.py
@@ -5,5 +5,7 @@ from apartments import views
 urlpatterns = [
     path("delete/<int:apt_id>/", views.delete_apartment),
     path("add/", views.add_apartment),
-    path("list/", views.ApartmentsAPIView.as_view()),
+    path("list/", views.get_apartments),
+    path("change/<int:apt_id>/", views.change_apartment),
+    path("delete/<int:apt_id>/", views.delete_apartment),
 ]
--- apartments/views.py
+++ apartments/views.py
@@ -1,25 +1,42 @@
-from django.http import Http404, HttpResponse
-from rest_framework import generics
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
 from apartments.models import Apartments
-from apartments.serializers import ApartmentsSerializer
-
-
-def delete_apartment(request, apt_id):
-    if not apt_id:
-        raise Http404()
-    pass
 
 
 @api_view(["POST"])
 def add_apartment(request):
-    new_apt = ApartmentsSerializer(data=request.data)
-    if new_apt.is_valid():
-        new_apt.save()
-        return HttpResponse(f"New apartment added: {new_apt.data}")
+    description = request.POST.get("description")
+    price = request.POST.get("price")
+    new_apt = Apartments(description=description, price=price)
+    new_apt.save()
+    return HttpResponse(f"New apartment added with id: {new_apt.pk}")
 
 
-class ApartmentsAPIView(generics.ListAPIView):
-    queryset = Apartments.objects.all()
-    serializer_class = ApartmentsSerializer
+@api_view(["GET"])
+def get_apartments(request):
+    apts = Apartments.objects.all()
+    data = [
+        {"id": apt.id, "description": apt.description, "price": apt.price}
+        for apt in apts
+    ]
+    return HttpResponse(data)
+
+
+@api_view(["PATCH"])
+def change_apartment(request, apt_id):
+    changed_apt = Apartments.objects.get(pk=apt_id)
+    description = request.data.get("description")
+    price = request.data.get("price")
+    if description:
+        changed_apt.description = description
+    if price:
+        changed_apt.price = price
+    changed_apt.save()
+    return HttpResponse(f"Apartment {apt_id} has been changed")
+
+
+@api_view(["DELETE"])
+def delete_apartment(request, apt_id):
+    Apartments.objects.filter(pk=apt_id).delete()
+    return HttpResponse(f"Apartment {apt_id} has been deleted")
