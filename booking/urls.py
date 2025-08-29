9cf6b3c3efa49f96fbbad2d75b1843e27dd664dd
dae22 <dae-22@mail.ru>
2025-08-29 04:13:22 +0300

    Some improves in booking views

--- booking/urls.py
+++ booking/urls.py
@@ -2,12 +2,4 @@ from django.urls import path
 
 from booking import views
 
-urlpatterns = [
-    path("add/", views.BookingAPIView.as_view(), name="add"),
-    path(
-        "check_booking/<int:apt_id>/",
-        views.BookingAPIView.as_view(),
-        name="check_booking",
-    ),
-    path("delete/<int:booking_id>/", views.BookingAPIView.as_view(), name="delete"),
-]
+urlpatterns = [path("", views.BookingAPIView.as_view())]
--- booking/views.py
+++ booking/views.py
@@ -2,7 +2,6 @@ from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 from apartments.models import Apartments
@@ -10,11 +9,8 @@ from booking.models import Booking
 
 
 class BookingAPIView(APIView):
-    authentication_classes = []  # disables authentication
-    permission_classes = []
-
-    def get(self, request, apt_id):
-        bookings = Booking.objects.filter(apartment_id=apt_id)
+    def get(self, request):
+        bookings = Booking.objects.filter(apartment_id=request.data.get("apartment_id"))
         if bookings.exists():
             data = [
                 {
@@ -51,48 +47,7 @@ class BookingAPIView(APIView):
                 status=status.HTTP_409_CONFLICT,
             )
 
-    def delete(self, request, booking_id):
-        booking = get_object_or_404(Booking, pk=booking_id)
+    def delete(self, request):
+        booking = get_object_or_404(Booking, pk=request.data.get("booking_id"))
         booking.delete()
         return JsonResponse({"message": "Booking deleted"}, status=status.HTTP_204_NO_CONTENT)
-
-
-# @api_view(["POST"])
-# def add_booking(request):
-#     apartment = get_object_or_404(Apartments, pk=request.data.get("apt_id"))
-#     start = request.data.get("start")
-#     end = request.data.get("end")
-#     cross_booking = Booking.objects.filter((Q(start__lte=start) & Q(end__gt=start)) |
-#                                               (Q(start__lt=end) & Q(end__gte=end)) |
-#                                               (Q(start__gte=start) & Q(end__lte=end)),
-#                                               apartment=apartment)
-#     if not cross_booking.exists():
-#         new_booking = Booking(apartment=apartment, start=start, end=end)
-#         new_booking.save()
-#         return JsonResponse({"message": f"New booking added with id: {new_booking.pk}"},
-#                             status=status.HTTP_201_CREATED)
-#     else:
-#         return JsonResponse({"error": "Booking dates cross with other booking"},
-#                             status=status.HTTP_409_CONFLICT)
-
-
-# @api_view(["GET"])
-# def get_booking(request, apt_id):
-#     bookings = Booking.objects.all()
-#     data = [
-#         {
-#             "booking_id": b.pk,
-#             "start_date": b.start.isoformat(),
-#             "end_date": b.end.isoformat(),
-#         }
-#         for b in bookings
-#     ]
-#     data.sort(key=lambda x: x["start_date"])
-#     return HttpResponse(data)
-
-
-# @api_view(["DELETE"])
-# def delete_booking(request, booking_id):
-#     booking = Booking.objects.get(pk=booking_id)
-#     booking.delete()
-#     return JsonResponse({"Delete booking": "success"})
